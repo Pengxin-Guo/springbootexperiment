067ebcde16e9be7ed74eddf044e0b5dfb66ebcfd
gpx333 <1457495424@qq.com>
2019-03-21 10:14:53 +0800

    add experiment02

---
+++ springboot-experiment-02/src/main/java/com/example/springbootexperiment02/entity/Address.java
@@ -0,0 +1,21 @@
+package com.example.springbootexperiment02.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String detail;
+    @ManyToOne
+    private User user;
+    @Column(columnDefinition = "TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP",
+            insertable = false
+            , updatable = false)
+    private LocalDateTime insertTime;
+
+    public Address(String detail) {
+        this.detail = detail;
+    }
+}
