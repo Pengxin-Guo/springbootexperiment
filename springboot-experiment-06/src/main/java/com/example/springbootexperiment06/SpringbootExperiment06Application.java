d022ce2e8083fc148201f9368ec71b2dea3fc8cf
gpx <1457495424@qq.com>
2019-04-26 10:29:18 +0800

    add experiment 07

---
+++ springboot-experiment-06/src/main/java/com/example/springbootexperiment06/SpringbootExperiment06Application.java
@@ -0,0 +1,20 @@
+package com.example.springbootexperiment06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+@SpringBootApplication
+public class SpringbootExperiment06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootExperiment06Application.class, args);
+    }
+    @Bean
+    public MethodValidationPostProcessor methodValidationPostProcessor() {
+        return new MethodValidationPostProcessor();
+    }
+
+}
